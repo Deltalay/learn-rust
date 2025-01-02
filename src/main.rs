77fbf2b779ba26f03f2af8d2da2c3e7b7cbea9ef
Deltalay <delta@DESKTOP-796GSE4.>
2025-01-02 12:01:39 +0700

    testing

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,9 @@ use learn_rust::generate_unique_string;
 
 fn main() {
     let unique = generate_unique_string(10, "Hello World");
+    let unique2 = generate_unique_string(14, "Hello World");
+    let unique3 = generate_unique_string(12, "Hello World");
     println!("{}", unique);
+    println!("{}", unique2);
+    println!("{}", unique3);
 }
